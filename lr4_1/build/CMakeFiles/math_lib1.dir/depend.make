c3587a3856a26957e31318f4404dc01a19ec2b8f
Kate <144523523+fehrale@users.noreply.github.com>
2024-12-24 16:05:58 +0300

    Delete lr4_1 directory

--- lr4_1/build/CMakeFiles/math_lib1.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for math_lib1.
-# This may be replaced when dependencies are built.
--- lr4_1/contracts.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONTRACTS_H
-#define CONTRACTS_H
-
-float E(int x);
-int* Sort(int* array, int size);
-
-#endif
